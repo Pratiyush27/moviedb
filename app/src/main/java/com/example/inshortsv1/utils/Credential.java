f886cce2c8d1ef5922f3f7ce65a9ba9c45e01df9
Pratiyush Prakash <prakashpratiyush@gmail.com>
2021-07-19 07:58:09 +0530

    Removed API KEY

--- app/src/main/java/com/example/inshortsv1/utils/Credential.java
+++ app/src/main/java/com/example/inshortsv1/utils/Credential.java
@@ -3,7 +3,7 @@ package com.example.inshortsv1.utils;
 public class Credential {
 
     public static final String Base = "https://api.themoviedb.org";
-    public static final String api_key = "a7af8141708a57cc201a930b0bf272bd";
+    public static final String api_key = "Add Your API KEY";
 
 
     public static final boolean POPULAR = true;
